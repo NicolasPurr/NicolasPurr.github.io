415ebd1cd7908f3aa0679c66ebc3ae576bd52442
nicolasPurr <123591418+NicolasPurr@users.noreply.github.com>
2026-03-11 20:27:01 +0100

    deploy: initial astro cv to subfolder

--- cv/astro.config.mjs
+++ cv/astro.config.mjs
@@ -6,6 +6,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://NicolasPurr.github.io', 
+    base: '/cv',
     vite: {
         plugins: [tailwindcss()],
     },
